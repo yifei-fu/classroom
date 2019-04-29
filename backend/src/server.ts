9c37120e26006ab1e293f74bb437cbb10ddbf538
yifei-fu <yifeifu2@gmail.com>
2019-04-29 00:55:33 -0700

    Frontend: add TypeORM and connect to mongodb.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,17 @@
 import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as express from 'express';
+import {createConnection} from 'typeorm';
 
 // get configurations from environment variables
 const port: number = Number(process.env.PORT) || 8080;
+
+createConnection({
+    type: 'mongodb',
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
 
 const app = express();
 
@@ -12,8 +20,8 @@ app.use(bodyParser.json());
 app.use(compression());
 
 // route API endpoints
-app.get('/', (req, res) => {
-    res.send('ok');
+app.get('/healthcheck', (req, res) => {
+    res.send();
 });
 
 // start Express app
