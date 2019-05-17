ff0ec0a2b7dd45c3264f841281ae96a9349a338e
JayKun <junkaiong11@gmail.com>
2019-05-16 19:55:06 -0700

    Add basic user entity

---
+++ backend/src/entity/User.ts
@@ -0,0 +1,26 @@
+import {Entity, PrimaryGeneratedColumn, Column, ObjectID, ObjectIdColumn} from "typeorm";
+
+@Entity()
+export class User {
+
+    @ObjectIdColumn()
+    id: ObjectID;
+
+    @Column()
+    username: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    isInstructor: boolean
+
+    @Column()
+    uid: string
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,35 @@
 import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as express from 'express';
-import {createConnection} from 'typeorm';
+import {createConnection, ConnectionOptions} from 'typeorm';
+import 'reflect-metadata'
+import {equal} from 'assert'
+import {User} from './entity/User'
 
 // get configurations from environment variables
 const port: number = Number(process.env.PORT) || 8080;
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
-createConnection({
-    type: 'mongodb',
-}).catch((err) => {
-    console.log(err);
-    process.exit(1);
+const options: ConnectionOptions = {
+    "type": 'mongodb',
+    entities: [User]
+}
+
+createConnection(options).then(connection => {
+    let user = new User()
+    user.firstName = 'test'
+    user.lastName = 'test'
+
+    let usersRepository = connection.getRepository(User)
+
+    usersRepository
+    .save(user)
+    .then(user=> console.log("User has been saved: ", user))
+    .catch(error => console.log("Cannot save. Error: ", error))
+
+}).catch(err => {
+    console.log(err)
+    process.exit(1)
 });
 
 const app = express();
@@ -22,6 +41,13 @@ app.use(compression());
 // route API endpoints
 app.get('/healthcheck', (req, res) => {
     res.send();
+});
+
+app.post('/user', urlencodedParser, (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let query = { username: username };
+
 });
 
 // start Express app
