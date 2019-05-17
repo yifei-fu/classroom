d571a5ab84ad51e9d2d2e5749ecc277a6277645e
Keshav Tadimeti <keshavtadimeti@gmail.com>
2019-05-17 12:20:31 -0700

    Fixed up UI of join class view

--- frontend/src/components/JoinCourseView.tsx
+++ frontend/src/components/JoinCourseView.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Form, Button } from 'react-bootstrap';
+
+var stuff: string | undefined;
 
 function JoinCourseView({ match }: { match: any }) {
     const id = match.params.id;
@@ -7,12 +9,33 @@ function JoinCourseView({ match }: { match: any }) {
     // get course name from server using
 
     return (
-    <div>
-        <h1>Course title: TBD </h1>
-        
-        <h2>Course id: {id}</h2>
-    </div>
+        <div>
+            <h1>Course title: TBD </h1>
+            <h2>Course id: {id}</h2>
+            <br></br>
+            <br></br>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="joinClassSecret" >
+                    <Form.Label style={{ justifyContent: "left" }}>Class code</Form.Label>
+                    <Form.Control type="text" value={stuff} placeholder="Code" onChange={handleChange}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Join class
+                </Button>
+            </Form>          
+        </div>
     );
+}
+
+function handleChange(event: any) {
+
+}
+
+function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(event);
+    alert('This feature isn\'t enabled yet, but stay tuned!');
+    event.preventDefault();
 }
 
 
