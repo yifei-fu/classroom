4cdcfe61a72c1355100aaca4df7a55e5b1cf6130
JayKun <junkaiong11@gmail.com>
2019-05-16 23:51:39 -0700

    Initial implementation of course api endpoints

--- backend/src/controllers/course.ts
+++ backend/src/controllers/course.ts
@@ -1,10 +1,11 @@
-import {getMongoManager} from "typeorm"
+import {getMongoManager, AdvancedConsoleLogger} from "typeorm"
 import {Course} from '../entity/Course'
 
 // Course Controller Class
 export class CourseController {
     public static getCourse(req, res, mgr) {
         let name = req.query.name
+
         mgr.findOne(Course, {name: name})
         .then(doc => {
             res.json(doc);
@@ -19,11 +20,31 @@ export class CourseController {
     }
 
     public static createCourse(req, res, mgr) {
+        let name = req.body.name;
+        let school = req.body.school;
+        let term = req.body.term;
+
+        let course = {
+            name: name,
+            school: school,
+            term: term
+        };
+
+        mgr.InsertOne(Course, course)
+        .then(() => {console.log("Course created")})
+        .catch(err => {console.log(err)});
     }
 
     public static enrollCourse(req, res, mgr) {
+        let secret = req.body.joinSecret
     }
 
     public static getProfiles(req, res, mgr) {
+        let coursename = req.query.class
+
+        mgr.find(Course, {name: coursename})
+        .then(doc => {
+            res.json(doc);
+        });
     }
 }
