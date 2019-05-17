dc1a135363b82b028f6a17ee07f818a63d6dd7d4
Adit Deshpande <adeshpande3@ucla.edu>
2019-05-16 20:48:04 -0700

    More progress for sign in page

--- frontend/src/components/AuthView.tsx
+++ frontend/src/components/AuthView.tsx
@@ -5,89 +5,96 @@ import "./AuthView.css";
 const AuthView: React.FC = () => {
     return (
         <div className="root">
-					<Form style={{ width: '500px' }}>
-						<Row>
-
-							<Form.Group controlId="formfullname">
-							<Form.Label>Full Name</Form.Label>
-						      <Form.Control placeholder="First name" />
-						      <Form.Control placeholder="Last name" />
-					    </Form.Group>
-					  </Row>
-
-						<Row>
-						  <Form.Group controlId="formUsername">
-						    <Form.Label>Username</Form.Label>
-						    <Form.Control type="username" placeholder="Enter username" />
-						  </Form.Group>
-
-						  <Form.Group controlId="formBasicEmail">
-						    <Form.Label>Email address</Form.Label>
-						    <Form.Control type="email" placeholder="Enter email" />
-						    <Form.Text className="text-muted">
-						      We'll never share your email with anyone else.
-						    </Form.Text>
-						  </Form.Group>
-
-					    <Form.Group as={Row}>
-					      <Form.Label column >
-					        Account Type
-					      </Form.Label>
-					      <Col >
-					        <Form.Check
-					          type="radio"
-					          label="Student"
-					          name="formStudentRadio"
-					          id="formStudentRadio"
-					        />
-					        <Form.Check
-					          type="radio"
-					          label="Professor"
-					          name="formProfessor"
-					          id="formProfessor"
-					        />
-					      </Col>
+					<Form>
+					  <Form.Row>
+					    <Form.Group  controlId="formGridName">
+					    <Col >
+                <Form.Label>Full Name</Form.Label>
+                  <Form.Control type="firstname" placeholder="First Name" />
+                  <Form.Control type="lastname" placeholder="Last Name" />  
+					    </Col >
 					    </Form.Group>
 
-					  </Row>
+					    <Form.Group controlId="formGridUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter username" />
+					    </Form.Group>
+					  </Form.Row>
 
-				  	<Form.Group controlId="formUID">
-					    <Form.Label>UID</Form.Label>
-					    <Form.Control type="UID" placeholder="Enter UCLA UID" />
+					<Form.Row>
+					  <Form.Group controlId="formGridEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
 					  </Form.Group>
 
-					  <Row>
-						  <Form.Group controlId="formBasicPassword">
-						    <Form.Label>Password</Form.Label>
-						    <Form.Control type="password" placeholder="Password" />
-						  </Form.Group>
+					  <Form.Group controlId="formGridRadios">
+                <Form.Label >
+                  Account Type
+                </Form.Label>
+                <Col >
+                  <Form.Check
+                    type="radio"
+                    label="Student"
+                    name="formStudentRadio"
+                    id="formStudentRadio"
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Professor"
+                    name="formProfessor"
+                    id="formProfessor"
+                  />
+                </Col>
+					  </Form.Group>
 
-						  <Form.Group controlId="formBasicPasswordConfirm">
-						    <Form.Label>Confirm Password</Form.Label>
-						    <Form.Control type="confirm_password" placeholder="Password" />
-						  </Form.Group>
-						</Row>
+					  <Form.Group controlId="formUID">
+					  		  <Form.Label>UID</Form.Label>
+		              <Form.Control type="UID" placeholder="Enter UCLA UID" />
+		        </Form.Group>
 
-						  <Button variant="primary" type="submit">
-						    Sign Up
-						  </Button>
+					</Form.Row>
 
-						  <Form.Group controlId="formUsername">
-						    <Form.Label>Username</Form.Label>
-						    <Form.Control type="username" placeholder="Enter username" />
-						  </Form.Group>
+					  <Form.Row>
+              <Form.Group controlId="formBasicPassword">
+              <Col >
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+               </Col >
+              </Form.Group>
 
-						  <Form.Group controlId="formBasicPassword">
-						    <Form.Label>Password</Form.Label>
-						    <Form.Control type="password" placeholder="Password" />
-						  </Form.Group>
-
+              <Form.Group controlId="formBasicPasswordConfirm">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="confirm_password" placeholder="Password" />
+              </Form.Group>
+					  </Form.Row>
 
 					  <Button variant="primary" type="submit">
-					    Sign In
+					    Sign Up
 					  </Button>
 
-					</Form>;
+						<Form.Row>
+              <Form.Group controlId="formUsername">
+              <Col >
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter username" />
+              </Col >
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+
+            <Button variant="primary" type="submit">
+              Sign In
+            </Button>
+
+
+					</Form>
         </div>
     );
 }
