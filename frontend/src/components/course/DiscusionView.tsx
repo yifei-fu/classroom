33e716acc09117215cd3ab2a604d62178ba9ae5c
yifei-fu <yifeifu2@gmail.com>
2019-05-25 22:22:46 -0700

    Frontend: DiscussionView displays posts and comments.

--- frontend/src/components/course/DiscusionView.tsx
+++ frontend/src/components/course/DiscusionView.tsx
@@ -1,12 +1,27 @@
 import React, {useEffect, useState} from 'react';
+import {CourseDetails, Post} from '../../api/type';
+
+import {Col, Container, ListGroup, Row} from 'react-bootstrap';
+import '../../common.css';
+import PostCard from './PostCard';
 
 export interface Props {
+  courseDetails: CourseDetails;
 }
 
 const DiscussionView: React.FC<Props> = (props: Props) => {
+  const {courseDetails: {posts}} = props;
   return (
     <div>
-      Discussion View
+      <Container fluid>
+        <Row>
+          {posts.map((post: Post, idx: number) => (
+            <Col md={12} lg={6} xl={4}>
+              <PostCard key={idx} post={post} />
+            </Col>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
 };
