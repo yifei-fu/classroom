9bd9edbd5666f9950d468b4e480298e653bc2916
JayKun <junkaiong11@gmail.com>
2019-06-04 21:53:21 -0700

    Add getUser

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,9 +1,11 @@
 import {getMongoManager, MongoEntityManager, TreeLevelColumn} from 'typeorm';
 import {Config} from '../config';
 import {User} from '../entity/User';
+import {UserProfile} from '../entity/UserProfile';
 import * as jwt from 'jwt-simple';
 import * as auth from './auth';
-
+import {UserProfileController} from './userprofile';
+import {AuthController} from './auth'
 const config: Config = require('../config.json')
 
 // User Controller Class
@@ -37,7 +39,7 @@ export class UserController {
         res.send();
     }
 
-    public static createUser(req, res) {
+    public static async createUser(req, res) {
         const {username, firstname, lastname, email, password, isInstructor, uid} = req.body
 
         if (!username || !firstname || !lastname || !email || !password || !isInstructor || !uid) {
@@ -46,7 +48,7 @@ export class UserController {
         }
 
         // Check whether user exists
-        getMongoManager().findOne(User, {username})
+        getMongoManager().findOne(User, {where: {$or: [{username}, {email}, {uid}]}})
         .then((doc) => {
             if (!doc) {
                 const newUser = {
@@ -64,28 +66,34 @@ export class UserController {
                 .then(() => console.log('User created'))
                 .catch((err) => {console.log(err); });
 
-                res.send(200, 'Successful operation');
-                /* TODO: When user is created, a user profile should be created as well
-                Call creatUserProfile()
-                */
+                /* When user is created, a user profile should be created as well*/
+                UserProfileController.createUserProfile(newUser).then((r)=>{
+                    console.log(r)
+                    res.status(200).send('User created')
+                });
             } else {
-                res.send(400, 'User already exists');
+                res.status(400).send('User already exists');
             }
         });
     }
 
-    public static getUser(req, res) {
-        /* TODO: Implement jwt */
-        // Identify user with jwt
-        const username = 'user';
+    public static async getUser(req, res) {
+        const token: string = req.body.token || req.headers['token'] || req.headers['x-access-token'];
+        if (!token) {
+            return res.status(400).send('Auth token missing')
+        }
 
-        getMongoManager().findOne(User, {username})
-        .then((doc) => {
-            console.log('Obtained User');
-            res.send();
-        })
-        .catch((err) => {
-            console.log(err);
+        const user = await AuthController.VerifyToken(token)
+        if (!user) {
+            return res.status(400).send('Invalid auth token')
+        }
+
+        console.log('user identified: ', user.uid)
+
+        getMongoManager().findOne(UserProfile, {uid: user.uid})
+        .then((profile)=>{
+            console.log(profile);
+            res.json(profile);
         });
     }
 
