5ab492a86d32c47e1e3c5a21b4475e45b0e43ec8
JayKun <junkaiong11@gmail.com>
2019-06-04 17:26:45 -0700

    Add AuthController

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,5 +1,6 @@
 import * as passport from 'passport';
 import * as passportJWT from 'passport-jwt';
+import * as jwt from 'jwt-simple';
 import {Config} from '../config';
 import {UserController} from './user';
 
@@ -38,3 +39,11 @@ export function auth() {
         }
     };
 };
+
+export class AuthController {
+    public static VerifyToken(token: string) {
+        const decode = jwt.decode(token, config.jwtSecret)
+        const uid = decode.uid
+        return UserController.getUserByUID(uid)
+    }
+}
